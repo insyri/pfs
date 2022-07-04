24808343bc43e5a65ac04e24ce8217bf6e322325
insyri <60794909+insyri@users.noreply.github.com>
2022-07-03 23:59:43 -0400

    added verbose, colors, gaurds, and help upds [ci skip]

--- start-pfs.sh
+++ start-pfs.sh
@@ -5,58 +5,112 @@ DEV_DSPEC="./dev-docker-compose.yml"
 CONFIG="./pfs.toml"
 ENV="prod"
 VERBOSE=false
-HELP="start-pfs
+COLOR="on"
+HELP_MAIN="start-pfs
 
 USAGE:
-    start-pfs.sh [FLAGS] [OPTION...]
+    start-pfs.sh <SUBCOMMAND> [FLAGS] [OPTION...]
+
+SUBCOMMANDS:
+    prepare                  Prepares the environment for pfs.
+    start                    Prepares and starts the pfs service.
 
 FLAGS:
-    -h, --help                Shows help information.
-    -e, --env    [dev|prod]   Use dev compose file.          default: prod
-    -c, --config FILE         Path to configuration file.    default: pfs.toml
+    -h, --help               Shows help information.
+    -e, --env    [dev|prod]  Use dev compose file.        default: prod
+    -c, --config string      Path to configuration file.  default: pfs.toml
+    -C, --color  [on|off]    Enable color output.         default: true
 
 OPTIONS:
-    -v, --verbose             Print verbose output.          default: false
-
-EXAMPLES:
-    start-pfs.sh -c example.toml
-    start-pfs.sh -c=example.toml
-    start-pfs.sh --env=prod
-    start-pfs.sh --config=spec.toml --verbose
+    -v, --verbose            Print verbose output.        default: false
 "
-OPTS=$(getopt -o e:c:vh --long env:,config:,verbose,help -n 'start-pfs' -- "$@")
+
+OPTS=$(getopt -o e:c:vhC: --long env:,config:,verbose,help,color: -n 'start-pfs' -- "$@")
 
 verbose_echo() {
     if [ "$VERBOSE" = true ]; then
-        echo "[VERBOSE]" "$@"
+        echo -e "$(if [ "$COLOR" = "on" ]; then echo -e "\e[1;33m[VERBOSE]\e[0m" "$@"; else echo "[VERBOSE]" "$@"; fi)"
     fi
 }
 
-spec_to_file() {
-    if [ "$1" = "prod" ]; then
-        return $PROD_DPSEC
-    elif [ "$1" = "dev" ]; then
-        return $DEV_DSPEC
-    else
-        echo "Invalid environment provided. Exiting."
-        exit 1
-    fi
-}
-# used to be $?
-if [ ! "$OPTS" != 0 ]; then echo "Failed parsing options." >&2 ; exit 1 ; fi
+if [ ! "$OPTS" != 0 ]; then
+    echo "Failed parsing options."
+    exit 1
+fi
 
 eval set -- "$OPTS"
 
 while true; do
     case "$1" in
-        -e | --env ) shift; if [ "$1" = "dev" ]; then ENV="dev"; fi; shift ;;
-        -c | --config ) shift; if [ ! -e "$1"  ]; then echo "Config file does not exist."; exit; fi; CONFIG="$1"; shift ;;
-        -h | --help ) echo "$HELP" && exit; shift ;;
-        -v | --verbose ) VERBOSE=true; shift ;;
-        -- ) shift; break ;;
-        * ) break ;;
+    -e | --env)
+        shift
+        if [ "$1" = "dev" ]; then
+            ENV="dev";
+        elif [ "$1" != "prod" ]; then
+            echo "Invalid environment. Valid environments are only dev and prod."
+            exit 1 
+        fi
+        shift
+        ;;
+    -C | --color)
+        shift
+        if [ "$1" = "off" ]; then
+            COLOR="off";
+        elif [ "$1" != "on" ]; then
+            echo "Invalid color. Valid colors are only on and off."
+            exit 1
+        fi
+        shift
+        ;;
+    -c | --config)
+        shift
+        if [ ! -e "$1" ]; then
+            echo "Config file does not exist."
+            exit
+        fi
+        CONFIG="$1"
+        shift
+        ;;
+    -h | --help)
+        echo "$HELP_MAIN" && exit
+        shift
+        ;;
+    -v | --verbose)
+        VERBOSE=true
+        shift
+        ;;
+    --)
+        shift
+        break
+        ;;
+    *) break ;;
     esac
 done
 
-FILE=$(spec_to_file $ENV)
-docker compose -f "$FILE" up
+verbose_echo "Recieved options:"
+verbose_echo "\$ENV: $ENV"
+verbose_echo "\$CONFIG: $CONFIG"
+verbose_echo "\$VERBOSE: $VERBOSE"
+verbose_echo "\$COLOR: $COLOR"
+if [ "$1" != "" ]; then verbose_echo "\$1: $1"; fi
+
+prepare() {
+    verbose_echo "Running prepare function"
+}
+
+run() {
+    verbose_echo "Running run function"
+    if [ "$ENV" = "prod" ]; then
+        docker compose -f "$PROD_DPSEC" up -d
+    else
+        docker compose -f "$DEV_DSPEC" up -d
+    fi
+}
+
+
+
+if [ "$1" = "prepare" ]; then
+    prepare
+fi
+
+# docker compose -f "$(if [ $ENV = "dev" ]; then echo $DEV_DSPEC; else echo "$PROD_DPSEC"; fi;)" up
